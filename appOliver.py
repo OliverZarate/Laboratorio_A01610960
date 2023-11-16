8342991300ca726081540303945970834acb6f9e
OliverZarate <145620271+OliverZarate@users.noreply.github.com>
2023-11-15 19:29:12 -0600

    Update appOliver.py

---
+++ appOliver.py
@@ -0,0 +1,11 @@
+import datetime
+import time
+import pandas as pd
+import streamlit as st
+from PIL import Image
+"""Generación de la webapp con streamlit"""
+#definir título
+st.title("Título: Analítica de Datos")
+#definir header
+st.header("Este es un header")
+st.subheader("Este es un header")
