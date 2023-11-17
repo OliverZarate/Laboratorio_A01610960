2ccffb57e72d3c1160933187f6f1d8f1dfc9ab6d
OliverZarate <145620271+OliverZarate@users.noreply.github.com>
2023-11-16 18:34:59 -0600

    Update appOliver.py

--- appOliver.py
+++ appOliver.py
@@ -21,3 +21,29 @@ st.header("Obtener informacion de ayuda python")
 st.help(range)
 st.header("widgets: ")
 st.subheader("Checkbox")
+# Checkbox
+if st.checkbox("Show/Hide"):
+    st.text("Mostrar u ocultar Widget")
+    st.subheader("Radio buttons")
+# Radio Buttons
+status=st.radio("Cual es su status", ("Activo", "Inactivo"))
+if status == "Activo":
+    st.succes("Estas activo")
+else: 
+    st.warning("Inactivo")
+    st.subheader("SelectBox")
+# SelectBox
+occupation = st.selectbox(
+"Tu Ocupacion", ["Programador", "Cientifico de datos", "BI", "Ingeniero de datos"]
+    )
+st.write("Opción seleccionada:", occupation)
+st.subheader("Multiselect")
+#Multiselect
+location=st.multiselect(
+    "Donde trabajas?.
+    ("México", "Nueva York", "Guadalajara", "Monterrey", "Nepal", "Buenos Aires", "Caracas"),
+)
+st.write("Seleccionó:", len(location), "locaciones")
+st.subheader("slider")
+# slider
+level=st.slider("Cual es tu nivel?", 1, 5)
